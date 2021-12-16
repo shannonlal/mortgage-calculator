b399fe587b77274e7bce54bda3048449d2a14afd
Shannon Lal <shannonlal@hotmail.com>
2021-12-15 20:58:14 -0500

    Check in base code for calculator-input component

--- libs/calculator-display/src/lib/calculator-display.module.ts
+++ libs/calculator-display/src/lib/calculator-display.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CalculatorInputComponent } from './calculator-input/calculator-input.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    CalculatorInputComponent
+  ],
+  exports: [
+    CalculatorInputComponent
+  ],
 })
 export class CalculatorDisplayModule {}
---
+++ libs/calculator-display/src/lib/calculator-input/calculator-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mortgage-calculator-calculator-input',
+  templateUrl: './calculator-input.component.html',
+  styleUrls: ['./calculator-input.component.scss']
+})
+export class CalculatorInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

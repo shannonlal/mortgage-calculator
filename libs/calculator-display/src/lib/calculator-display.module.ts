e11c422f17f488459906345dbd2e2e9ac863cc94
Shannon Lal <shannonlal@hotmail.com>
2021-12-15 21:30:43 -0500

    Check in basic Material Design UI

--- libs/calculator-display/src/lib/calculator-display.module.ts
+++ libs/calculator-display/src/lib/calculator-display.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { CalculatorInputComponent } from './calculator-input/calculator-input.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   declarations: [
     CalculatorInputComponent

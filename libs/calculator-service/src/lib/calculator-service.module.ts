a40d873b5b2f03dcc672aac9bc456bfbf86eea09
Shannon Lal <shannonlal@hotmail.com>
2021-12-19 21:54:37 -0500

    Check in new term service

--- libs/calculator-service/src/lib/calculator-service.module.ts
+++ libs/calculator-service/src/lib/calculator-service.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TermService } from './term/term.service';
 
 @Module({
   controllers: [],
   providers: [],
-  exports: [],
+  exports: [ TermService ],
 })
 export class CalculatorServiceModule {}

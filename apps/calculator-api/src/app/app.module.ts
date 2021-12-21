c8ef837aae5e6815823dfc4712bb3cbeb0945e1e
Shannon Lal <shannonlal@hotmail.com>
2021-12-20 21:51:49 -0500

    Adding Payment Frequency Controller with tests

--- apps/calculator-api/src/app/app.module.ts
+++ apps/calculator-api/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { TermController } from './controllers/term/term.controller';
 import { CalculatorServiceModule } from '@mortgage-calculator/calculator-service';
 import { InterestRateController } from './controllers/interest-rate/interest-rate.controller';
 import { AmortizationController } from './controllers/amortization/amortization.controller';
+import { PaymentFrequencyController } from './controllers/payment-frequency/payment-frequency.controller';
 
 @Module({
   imports: [CalculatorServiceModule],
-  controllers: [TermController, InterestRateController, AmortizationController],
+  controllers: [TermController, InterestRateController, AmortizationController, PaymentFrequencyController],
   providers: [],
 })
 export class AppModule {}
---
+++ apps/calculator-api/src/app/controllers/payment-frequency/payment-frequency.controller.spec.ts
@@ -0,0 +1,52 @@
+import { PaymentFrequencyService } from '@mortgage-calculator/calculator-service';
+import { PaymentFrequency } from '@mortgage-calculator/models';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PaymentFrequencyController } from './payment-frequency.controller';
+
+describe('PaymentFrequencyController', () => {
+  let controller: PaymentFrequencyController;
+
+  const mockPaymentFrequency: Array<PaymentFrequency> = [
+    {
+      "label": "Weekly",
+      "weeks": 52
+    },
+    {
+        "label": "Accelerated Bi-Weekly",
+        "weeks": 26
+    }
+  ];
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PaymentFrequencyController],
+      providers: [
+        {
+          provide: PaymentFrequencyService,
+          useValue: {
+            getPaymentFrequency: jest.fn(() => {
+              return Promise.resolve(mockPaymentFrequency);
+            }),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<PaymentFrequencyController>(PaymentFrequencyController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should get payment frequency', async () => {
+    try{
+      const paymentFrequency:Array<PaymentFrequency> = await controller.getPaymentFrequency();
+      expect( paymentFrequency ).toBeDefined();
+      expect( paymentFrequency.length ).toBe( 2 );
+      expect( paymentFrequency[0].label ).toBe( 'Weekly' );
+      expect( paymentFrequency[0].weeks ).toBe( 52 );
+    } catch (err){
+      expect( err ).toBeUndefined();
+    }
+  });
+});

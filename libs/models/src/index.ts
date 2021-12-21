0ba388d712e21cbb26a1a73ae1e55144cbc686ff
Shannon Lal <shannonlal@hotmail.com>
2021-12-20 21:28:38 -0500

    Check in latest payment frequency

--- libs/models/src/index.ts
+++ libs/models/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/term';
 export * from './lib/interest-rate';
 export * from './lib/amortization';
+export * from './lib/payment-frequency';
---
+++ libs/models/src/lib/payment-frequency.ts
@@ -0,0 +1,5 @@
+export interface PaymentFrequency {
+  label: string;
+  weeks: number;
+};
+

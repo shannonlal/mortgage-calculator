7e43617225c92c25cc9024b705c99989c528a1e3
Shannon Lal <shannonlal@hotmail.com>
2021-12-20 20:40:07 -0500

    Check in service get amaortization years

---
+++ libs/calculator-service/src/lib/amortization/amortization.service.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AmortizationService } from './amortization.service';
+
+describe('AmortizationService', () => {
+  let service: AmortizationService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AmortizationService],
+    }).compile();
+
+    service = module.get<AmortizationService>(AmortizationService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should get Amortization Years', async () => {
+    try{
+      const amortizationYears = await service.getAmortizationYears();
+
+      expect( amortizationYears ).toBeDefined();
+      expect( amortizationYears.length ).toBe( 30 );
+      expect( amortizationYears[0].label ).toBe( '1 Year');
+      expect( amortizationYears[0].year).toBe( 1 );
+      expect( amortizationYears[1].label ).toBe( '2 Years');
+      expect( amortizationYears[1].year).toBe( 2 );
+    }catch( err ){
+      expect( err ).toBeUndefined();
+    }
+  });
+});

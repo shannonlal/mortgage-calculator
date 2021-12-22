9146513c3950f9995d52828fa7ab833e31d40680
Shannon Lal <shannonlal@hotmail.com>
2021-12-21 22:29:55 -0500

    Check in string input and test cases

--- libs/shared-ui-react/src/lib/input/input.tsx
+++ libs/shared-ui-react/src/lib/input/input.tsx
@@ -51,7 +51,39 @@ const NumericCalculatorInput = ( props: NumericCalculatorInputProps ) =>{
         }
 };
 
+const StringCalculatorInput = ( props: StringCalculatorInputProps ) =>{
+
+    const change = ( e: React.ChangeEvent<any> ) => {
+        props.handleChange( props.eventName, e.target.value);
+    }
+
+    if( props.adornmentPosition && props.adornmentValue){
+        return (
+            <OutlinedInput
+                    id={props.id}
+                    data-testid={props.id}
+                    value={props.value}
+                    fullWidth
+                    onChange={(e) => change(e,)}
+                    startAdornment={<InputAdornment position={props.adornmentPosition}>{props!.adornmentValue}</InputAdornment>}
+                />
+          );
+        } else {
+            return (
+                <OutlinedInput
+                    id={props.id}
+                    data-testid={props.id}
+                    value={props.value}
+                    fullWidth
+                    onChange={(e) => change(e,)}
+                />
+            );
+        }
+};
+
 export {
     NumericCalculatorInput,
-    NumericCalculatorInputProps
+    NumericCalculatorInputProps,
+    StringCalculatorInput,
+    StringCalculatorInputProps
 };

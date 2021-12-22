ecff36c208431a15992465b07021d775e7a7328f
Shannon Lal <shannonlal@hotmail.com>
2021-12-21 22:23:14 -0500

    Check in latest test cases

--- libs/shared-ui-react/src/lib/input/input.spec.tsx
+++ libs/shared-ui-react/src/lib/input/input.spec.tsx
@@ -27,4 +27,46 @@ describe("when rendered with a Numeric Calculator Input", () => {
       expect( err ).toBeUndefined();  
     }
   });
+
+  it("should render Numeric Calculator Input with % adornment", async () => {
+
+    const handleChange = jest.fn();
+    const props: NumericCalculatorInputProps = {
+        id: 'component-id',
+        value: 100.01,
+        handleChange,
+        eventName: 'component-event',
+        adornmentValue: '%', 
+        adornmentPosition: 'start'
+    };
+    try{
+        const { queryByText } = render(<NumericCalculatorInput {...props} />); 
+
+        const input: HTMLElement| null = await queryByText('%')
+
+        expect( input ).toBeDefined();
+    } catch( err ){
+      expect( err ).toBeUndefined();  
+    }
+  });
+
+  it("should render Numeric Calculator Input without % adornment", async () => {
+
+    const handleChange = jest.fn();
+    const props: NumericCalculatorInputProps = {
+        id: 'component-id',
+        value: 100.01,
+        handleChange,
+        eventName: 'component-event',
+    };
+    try{
+        const { queryByText } = render(<NumericCalculatorInput {...props} />); 
+
+        const input: HTMLElement| null = queryByText('%');
+
+        expect( input ).toBe( null );
+    } catch( err ){
+      expect( err ).toBeUndefined();  
+    }
+  });
 });

c79661413223bb0c3e056e1e2cab6f5db08a9e18
Shannon Lal <shannonlal@hotmail.com>
2021-12-31 14:52:16 -0500

    Check in first api service with tests

--- libs/shared-ui-services/src/lib/shared-ui-services.spec.ts
+++ libs/shared-ui-services/src/lib/shared-ui-services.spec.ts
@@ -1,7 +1,38 @@
-import { sharedUiServices } from './shared-ui-services';
+import mockAxios from 'jest-mock-axios';
+import { Term } from '@mortgage-calculator/models';
 
-describe('sharedUiServices', () => {
-  it('should work', () => {
-    expect(sharedUiServices()).toEqual('shared-ui-services');
+import { getTerms } from './shared-ui-services';
+
+jest.mock('axios');
+
+describe('getTerms', () => {
+
+  afterEach(() => {
+    // cleaning up the mess left behind the previous test
+    mockAxios.reset();
   });
+  it('should be defined', () => {
+    expect(getTerms).toBeDefined();
+  });
+
+  it('should get a list of terms', async () =>{
+
+    const t: Array<Term> =  [
+      {
+          "label": "1 Year",
+          "value": 1
+      },
+      {
+          "label": "2 Years",
+          "value": 2
+    }];
+
+    mockAxios.get.mockResolvedValueOnce(t);
+
+    const terms: Array<Term> = await getTerms( 'http://localhost:8080' );
+
+    expect( terms ).toBeDefined();
+
+    expect( terms.length ).toBe( 2 );
+  })
 });
--- libs/shared-ui-services/src/lib/shared-ui-services.ts
+++ libs/shared-ui-services/src/lib/shared-ui-services.ts
@@ -1,3 +1,16 @@
-export function sharedUiServices(): string {
-  return 'shared-ui-services';
+import { Term } from "@mortgage-calculator/models";
+import axios from "axios";
+
+// URL Definition
+const GET_TERMS_URL = `/api/term`;
+
+/**
+ * The following function will make a call to get the list of terms
+ * @param connection 
+ * @returns Array<Term>
+ */
+export const getTerms = async ( baseUrl: string ): Promise<Array<Term>> => {
+    const terms: Array<Term> = await axios.get( `${baseUrl}${GET_TERMS_URL}` );
+
+    return terms;
 }

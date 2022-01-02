e7cb9fe9d5a53e46128b3fbc3e42ba4b3d6c6810
Shannon Lal <shannonlal@hotmail.com>
2022-01-02 13:47:31 -0500

    Check in latest rate type integration to store (#26)

--- apps/calculator-ui-react/src/app/store/fixeddata/fixedData.effect.ts
+++ apps/calculator-ui-react/src/app/store/fixeddata/fixedData.effect.ts
@@ -1,11 +1,12 @@
-import { Term } from '@mortgage-calculator/models';
-import { getTerms } from '@mortgage-calculator/shared-ui-services';
+import { RateType, Term } from '@mortgage-calculator/models';
+import { getTerms, getRateTypes } from '@mortgage-calculator/shared-ui-services';
 import { store } from '../store';
 import { startLoadingTerms, loadTermsSuccess, loadTermsError } from './termsData.slice';
+import { startLoadingRateType, loadRateTypeSuccess, loadRateTypeError } from './rateType.slice';
 
 const baseUrl = `http://localhost:3333`;
 /**
- * 
+ * The following method will get the terms and load them into the store
  * @returns 
  */
 export const fetchTerms = async (): Promise<void> => {
@@ -17,5 +18,23 @@ export const fetchTerms = async (): Promise<void> => {
         store.dispatch( loadTermsSuccess( terms ));
     }catch(err){
         store.dispatch( loadTermsError( `Unexpected error loading terms` ));
+    }
+}
+
+/**
+ * The following method will get the terms and load them into the store
+ * @returns 
+ */
+ export const fetchRateTypes = async (): Promise<void> => {
+    try{
+        store.dispatch( startLoadingRateType());
+
+        const rateTypes: Array<RateType> = await getRateTypes( baseUrl );
+
+        const rateTypeNames: Array<string> = rateTypes.map (rt => rt.toString())
+
+        store.dispatch( loadRateTypeSuccess( rateTypeNames ));
+    }catch(err){
+        store.dispatch( loadRateTypeError( `Unexpected error loading rate types` ));
     }
 }

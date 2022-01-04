f17e3cc4eab7e3ea40e41c0423a979904c4af52a
Shannon Lal <shannonlal@hotmail.com>
2022-01-04 15:34:43 -0500

    Check in new model for getting mortgage result

---
+++ libs/models/src/lib/mortgage-result.spec.ts
@@ -0,0 +1,13 @@
+import { MortgageResult } from './mortgage-result';
+
+describe('MortgageResult', () => {
+  it('should validate interface MortgageResult', () => {
+    const mortgageResult: MortgageResult = {
+        monthlyPayment: 1231123.01,
+        id: "1231233213",
+        creationDate: new Date()
+    };
+    expect(mortgageResult).toBeDefined();
+    expect(mortgageResult.monthlyPayment).toBe(1231123.01);
+  });
+});
---
+++ libs/models/src/lib/mortgage-result.ts
@@ -0,0 +1,5 @@
+export interface MortgageResult {
+    id: string;
+    monthlyPayment: number;
+    creationDate: Date;
+}

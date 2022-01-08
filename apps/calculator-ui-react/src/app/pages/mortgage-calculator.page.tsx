1acb18503296da272ea5d2109171fcb033b99969
Shannon Lal <shannonlal@hotmail.com>
2022-01-08 14:14:57 -0500

    Check in code refactoring for ui

--- apps/calculator-ui-react/src/app/pages/mortgage-calculator.page.tsx
+++ apps/calculator-ui-react/src/app/pages/mortgage-calculator.page.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import {CalculatorInputForm, CalculatorInputFormProps } from '../components/calculator-input-form/calculator-input-form'
+import { CalculatorInputForm, CalculatorInputFormProps } from '../components/calculator-input-form/calculator-input-form';
+import { CalculationSummary, CalculationSummaryProps } from '../components/calculation-summary/calculation-summary';
 import { MortgageDetails, RateType } from "@mortgage-calculator/models";
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { fetchMortgageCalculation } from  '../store/mortgage-calculation/mortgage.effect';
@@ -48,26 +49,47 @@ const MortgageCalculator = () => {
       term: 5,
     });
 
+    const [displaySummaryTable, setDisplaySummaryTable] = useState<boolean>(false);
+
     const handleChange = (eventName: string, eventValue :number|string) => {
      dispatchMortgageDetails( eventName, eventValue, mortgageDetails)
     };
 
     const submitCalculateMortgage = async () => {
-      fetchMortgageCalculation(mortgageDetails);
+      await fetchMortgageCalculation(mortgageDetails);
+      setDisplaySummaryTable( true );
     }
 
     const [interestDetails, setInterestDetails] = useState({
-    totalAmountInPeriod: 0,
-    totalInterestInPeriod: 0,
-    mortgageAmountPerMonth: 0,
-    termInterestAmount: 0,
-    termPrincipleAmount: 0
-  });
+      totalAmountInPeriod: 0,
+      totalInterestInPeriod: 0,
+      mortgageAmountPerMonth: 0,
+      termInterestAmount: 0,
+      termPrincipleAmount: 0
+    });
 
   // calculate mortgage on initial render
   useEffect(() => {
     loadMortgageInitialParameters();
   }, []);
+
+  const renderSummaryTable = () => {
+    if( displaySummaryTable ){
+      const summaryTableProps: CalculationSummaryProps = {
+        term: mortgageDetails.term,
+        amortizationPeriodYear: mortgageDetails.amortizationYear,
+        mortgageAmountPerMonth: 0,
+        prepaymentAmount: mortgageDetails.prepaymentAmount,
+        termPrincipleAmount: 0,
+        mortgageAmount: mortgageDetails.mortgageAmount,
+        termInterestAmount: 0,
+        totalInterestInPeriod: 0,
+        totalAmountInPeriod: 0
+      };
+      return <CalculationSummary {...summaryTableProps} />
+    }
+    return;
+  }
 
   const inputPropoForm: CalculatorInputFormProps = {
     ...mortgageDetails,
@@ -76,8 +98,13 @@ const MortgageCalculator = () => {
     submitCalculateMortgage
   }
 
+  
+
   return (
-    <CalculatorInputForm {...inputPropoForm}/>
+    <>
+      <CalculatorInputForm {...inputPropoForm}/>
+      {renderSummaryTable()}
+    </>
   );
 };
 

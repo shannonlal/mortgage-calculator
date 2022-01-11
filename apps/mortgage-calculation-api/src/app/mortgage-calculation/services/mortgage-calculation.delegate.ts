5f5baf484af38c3ae966f7a0e110fe20011835f4
Shannon Lal <shannonlal@hotmail.com>
2022-01-10 22:43:02 -0500

    Check in latest functions

--- apps/mortgage-calculation-api/src/app/mortgage-calculation/services/mortgage-calculation.delegate.ts
+++ apps/mortgage-calculation-api/src/app/mortgage-calculation/services/mortgage-calculation.delegate.ts
@@ -39,3 +39,38 @@ export const getPeriodInterestPerMonth = (periodInsterestPerYear: number, paymen
 export const getTotalAmountPerPeriod = ( totalPeriodInterest: number, mortgageAmount: number, prepaymentAmount: number ): number => {
     return ( Number(totalPeriodInterest)+ Number( mortgageAmount - prepaymentAmount ));
 }
+
+/**
+ * 
+ * @param mortgageAmount 
+ * @param prepaymentAmount 
+ * @param amortizationPeriodYear 
+ * @param interestRate 
+ * @returns 
+ */
+export const getMortgageAmountYear = (mortgageAmount: number, prepaymentAmount: number, amortizationPeriodYear: number, interestRate: number ): number => {
+    return ((mortgageAmount - prepaymentAmount) * 100) /
+                (amortizationPeriodYear * 100 + interestRate * (amortizationPeriodYear * ((amortizationPeriodYear - 1) / 2)));
+}
+
+export const getTermPrincipleAmount = (mortgageAmountPerYear: number, term: number) :number => {
+    return (mortgageAmountPerYear * term);
+};
+
+/**
+ * 
+ * @param periodInsterestPerYear 
+ * @param term 
+ * @returns 
+ */
+export const getTermInterestAmount = (periodInsterestPerYear: number, term: number): number => {
+    return (periodInsterestPerYear * term );
+};
+
+export const getMortgageAmountPerMonth = (mortgageAmountPerYear: number, paymentFrequency: number ): number => {
+    return ( mortgageAmountPerYear/paymentFrequency );
+}
+
+export const getPeriodInstallmentAmount = (mortgageAmountPerMonth: number, periodInsterestPerMonth:number ) => {
+    return (mortgageAmountPerMonth + periodInsterestPerMonth);
+}

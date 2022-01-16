1bfd1fb42bb22fbc72362303910819cc1c4f9b29
Shannon Lal <shannonlal@hotmail.com>
2022-01-15 23:02:41 -0500

    Check in latest test cases for interest per month

--- apps/mortgage-calculation-api/src/app/mortgage-calculation/services/mortgage-calculation.delegate.spec.ts
+++ apps/mortgage-calculation-api/src/app/mortgage-calculation/services/mortgage-calculation.delegate.spec.ts
@@ -1,6 +1,6 @@
 
 
-import { getTotalPeriodInterest } from './mortgage-calculation.delegate';
+import { getPeriodInsterestPerYear, getPeriodInterestPerMonth, getTotalPeriodInterest } from './mortgage-calculation.delegate';
 
 describe('Mortgage Calculation Delegate', () => {
 
@@ -18,6 +18,47 @@ describe('Mortgage Calculation Delegate', () => {
         const totalPeriodInterest = getTotalPeriodInterest( mortgageAmount, prepaymentAmount, interestRate, amortizationPeriodYear);
 
         expect( totalPeriodInterest ).toBe( 18000 );
-      });
+    });
+
+    it('get Total Period Interest should exist with different values', async() => {
+        const mortgageAmount = 1000000;
+        const prepaymentAmount = 10000;
+        const interestRate = 5;
+        const amortizationPeriodYear = 20;
+        
+        const totalPeriodInterest = getTotalPeriodInterest( mortgageAmount, prepaymentAmount, interestRate, amortizationPeriodYear);
+
+        expect( totalPeriodInterest ).toBe( 990000 );
+    });
+  });
+
+  describe('getTotalPeriodInterest', () => {
+    it('get Total Period Interest should exist', async() => {
+      expect(getTotalPeriodInterest).toBeDefined();
+    });
+
+    it('getPeriodInsterestPerYear', async() => {
+        const totalPeriodInterest = 990000;
+        const amortizationPeriodYear = 20;
+        
+        const periodInsterestPerYear = getPeriodInsterestPerYear( totalPeriodInterest, amortizationPeriodYear);
+
+        expect( periodInsterestPerYear ).toBe( 49500 );
+    });
+  });
+
+  describe('getPeriodInterestPerMonth', () => {
+    it('get Total Period Interest should exist', async() => {
+      expect(getPeriodInterestPerMonth).toBeDefined();
+    });
+
+    it('getPeriodInterestPerMonth', async() => {
+        const periodInsterestPerYear = 49500;
+        const amortizationPeriodYear = 12;
+        
+        const periodInsterestPerMonth = getPeriodInterestPerMonth( periodInsterestPerYear, amortizationPeriodYear);
+
+        expect( periodInsterestPerMonth ).toBe( 4125 );
+    });
   });
 });

ca65d0b0a799d73f176b3db4b82f7ac61887d832
Shannon Lal <shannonlal@hotmail.com>
2022-01-16 14:13:44 -0500

    Check in basic calculation in service

--- apps/mortgage-calculation-api/src/app/mortgage-calculation/services/mortgage-calculation.service.ts
+++ apps/mortgage-calculation-api/src/app/mortgage-calculation/services/mortgage-calculation.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { CalculationResult } from '../dto/calculation-result.output';
 import { MortgageDetailsInput } from '../dto/mortgage-details.input';
+import { getMortgageAmountPerMonth, getMortgageAmountYear, getPeriodInstallmentAmount, getPeriodInsterestPerYear, getPeriodInterestPerMonth, getTermInterestAmount, getTermPrincipleAmount, getTotalPeriodInterest } from './mortgage-calculation.delegate';
 
 @Injectable()
 export class MortgageCalculationService {
@@ -12,19 +13,41 @@ export class MortgageCalculationService {
      * @param inputData 
      * @returns 
      */
-    async calculateMortgae(inputData: MortgageDetailsInput): Promise<CalculationResult> {
+    async calculateMortgae(mortgageDetails: MortgageDetailsInput): Promise<CalculationResult> {
         try{
-            this.logger.log(`Calculating the mortgage amount ${JSON.stringify(inputData)}`);
+            this.logger.log(`Calculating the mortgage amount ${JSON.stringify(mortgageDetails)}`);
 
+            const totalInterestInPeriod = getTotalPeriodInterest( 
+                mortgageDetails.mortgageAmount, 
+                mortgageDetails.prepaymentAmount, 
+                mortgageDetails.interestRate, 
+                mortgageDetails.amortizationYear);
+
+            const mortgageAmountPerYear = getMortgageAmountYear( 
+                mortgageDetails.mortgageAmount, 
+                mortgageDetails.prepaymentAmount,
+                mortgageDetails.amortizationYear,
+                mortgageDetails.interestRate );
+
+            const mortgageAmountPerMonth = getMortgageAmountPerMonth( mortgageAmountPerYear, mortgageDetails.paymentFrequency );
+
+            const termPrincipleAmount = getTermPrincipleAmount( mortgageAmountPerYear, mortgageDetails.term );
+
+            const periodInterestPerYear = getPeriodInsterestPerYear( totalInterestInPeriod, mortgageDetails.amortizationYear);
+            const termInterestAmount = getTermInterestAmount(periodInterestPerYear, mortgageDetails.term);
+            const periodInsterestPerMonth = getPeriodInterestPerMonth( periodInterestPerYear, mortgageDetails.paymentFrequency);
+
+            const totalAmountInPeriod = getPeriodInstallmentAmount( mortgageAmountPerMonth, periodInsterestPerMonth );
+            
             const calculation: CalculationResult = {
                 id: `1231233213`,
                 monthlyPayment: 1212.31,
                 creationDate: new Date().toISOString(),
-                mortgageAmountPerMonth: 1212.31,
-                termPrincipleAmount: 10000,
-                termInterestAmount: 3000,
-                totalInterestInPeriod: 400,
-                totalAmountInPeriod: 3000
+                mortgageAmountPerMonth,
+                termPrincipleAmount,
+                termInterestAmount,
+                totalInterestInPeriod,
+                totalAmountInPeriod
             }
             return Promise.resolve( calculation );
         }catch( err ){

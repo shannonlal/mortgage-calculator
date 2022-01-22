1ff91b14722841ea1c78f5795163ff48d9f23349
Shannon Lal <shannonlal@hotmail.com>
2022-01-21 23:02:12 -0500

    Check in switch in change port

--- apps/mortgage-summary-api/src/main.ts
+++ apps/mortgage-summary-api/src/main.ts
@@ -2,6 +2,7 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
+ const port = process.env.MORTGAGE_SUMMARY_PORT || 3335;
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
@@ -12,7 +13,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }

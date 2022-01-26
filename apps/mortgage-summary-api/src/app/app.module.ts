81e8a647ba6c9ad78c6148489acdc406df33cac6
Shannon Lal <shannonlal@hotmail.com>
2022-01-25 22:46:14 -0500

    Check in basic mongodb

--- apps/mortgage-summary-api/src/app/app.module.ts
+++ apps/mortgage-summary-api/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
 import { HealthCheckController } from './controllers/healthcheck.controller';
-import { HealthCheckService } from './services/health-check.service'
+import { HealthCheckService } from './services/health-check.service';
+import { MortgageSummaryModule } from '../mortgage-summary/mortgage-summary.module'
+
 const MONGO_URL = process.env.MONGODB_URL || 'mongodb://localhost/mortgage-summary';
 
 @Module({
-  imports: [MongooseModule.forRoot(MONGO_URL)],
+  imports: [
+    MongooseModule.forRoot(MONGO_URL),
+    MortgageSummaryModule
+  ],
   controllers: [HealthCheckController],
   providers: [HealthCheckService],
 })
--- apps/mortgage-summary-api/src/mortgage-summary/mortgage-summary.module.ts
+++ apps/mortgage-summary-api/src/mortgage-summary/mortgage-summary.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MortgageSummary, MortgageSummarySchema } from './schemas/mortgage-summary.schema'
+import { MortgageSummary, MortgageSummarySchema } from './schemas/mortgage-summary.schema';
+import { MortgageSummaryService } from './services/mortgage-summary.service';
+import { MortgageSummaryController} from './controllers/mortgage-summary.controller';
 
 @Module({
-
-    imports: [
-      MongooseModule.forFeature([{ name: MortgageSummary.name, schema: MortgageSummarySchema }]),
-    ],
+  providers: [MortgageSummaryService],
+  controllers: [MortgageSummaryController],
+  imports: [
+    MongooseModule.forFeature([{ name: MortgageSummary.name, schema: MortgageSummarySchema }]),
+  ],
   })
 export class MortgageSummaryModule {}

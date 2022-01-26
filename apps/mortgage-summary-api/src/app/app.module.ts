69a5a48c8c37817c482c66842b04075a673ef2fb
Shannon Lal <shannonlal@hotmail.com>
2022-01-25 22:21:21 -0500

    Check in mortgage summary module

--- apps/mortgage-summary-api/src/app/app.module.ts
+++ apps/mortgage-summary-api/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { MortgageSummaryController } from './controllers/mortgage-summary.controller';
 import { MortgageSummaryService } from './services/mortgage-summary.service';
 
+const MONGO_URL = process.env.MONGODB_URL || 'mongodb://localhost/mortgage-summary';
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(MONGO_URL)],
   controllers: [MortgageSummaryController],
   providers: [MortgageSummaryService],
 })
---
+++ apps/mortgage-summary-api/src/mortgage-summary/models/mortgage-summary.ts
@@ -0,0 +1,41 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type MortgageSummaryDocument = MortgageSummary & Document;
+
+@Schema()
+export class MortgageSummary {
+
+    id: string;
+
+    @Prop({ required: true })
+    monthlyPayment: number;
+
+    @Prop({ required: true })    
+    creationDate: string;
+
+    @Prop({ required: true })
+    mortgageAmountPerMonth: number;
+
+    @Prop({ required: true })
+    termPrincipleAmount: number;
+
+    @Prop({ required: true })
+    termInterestAmount: number;
+
+    @Prop({ required: true })
+    totalInterestInPeriod: number;
+
+    @Prop({ required: true })
+    brokerName: string;
+
+    @Prop({ required: true })
+    clientName: string;
+
+    @Prop({ required: true })
+    createDate: Date;
+}
+
+
+
+export const MortgageSummarySchema = SchemaFactory.createForClass(MortgageSummary);

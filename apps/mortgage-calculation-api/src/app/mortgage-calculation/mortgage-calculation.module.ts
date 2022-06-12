f817213f438fe7e5f1a5a7af3a5371274a6b9be6
Shannon Lal <shannonlal@hotmail.com>
2022-06-12 14:28:44 -0400

    Defining resolver for about mortgage

--- apps/mortgage-calculation-api/src/app/mortgage-calculation/mortgage-calculation.module.ts
+++ apps/mortgage-calculation-api/src/app/mortgage-calculation/mortgage-calculation.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MortgageCalculationResolver } from './resolvers/mortgage-calculation.resolver';
 import { MortgageCalculationService } from './services/mortgage-calculation.service';
 import { MortgageAboutService } from './services/mortgage-about.service';
+import { MortgageAboutResolver } from './resolvers/mortgage-about.resolver';
 
 @Module({
   providers: [
     MortgageCalculationResolver, 
     MortgageCalculationService,
+    MortgageAboutResolver,
     MortgageAboutService 
   ],
 })
